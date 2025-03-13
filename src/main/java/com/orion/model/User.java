f96d4c70692a4690d132dd6a714f0485c8a3cf94
fearrux <tembulatbekov7@gmail.com>
2025-03-13 07:49:26 +0300

    feat: add user model

---
+++ src/main/java/com/orion/model/User.java
@@ -0,0 +1,25 @@
+package com.orion.model;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+@Entity
+@Table(name = "users")
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+@Getter
+@Setter
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    private Long id;
+
+    @Column(name = "login", nullable = false, unique = true)
+    private String login;
+
+    @Column(name = "password", nullable = false)
+    private String password;
+}
