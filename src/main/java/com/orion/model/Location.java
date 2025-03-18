36f0231c48bee0ecfc2a24f276105a2bc11baf1b
orion2403 <tembulatbekov7@gmail.com>
2025-03-18 20:50:41 +0300

    style: change imports, use absolute path instead using *

--- src/main/java/com/orion/model/Location.java
+++ src/main/java/com/orion/model/Location.java
@@ -1,7 +1,20 @@
 package com.orion.model;
 
-import jakarta.persistence.*;
-import lombok.*;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
+import jakarta.persistence.Id;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Column;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.JoinColumn;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.math.BigDecimal;
 
 @Entity
 @Table(name = "locations")
--- src/main/java/com/orion/model/User.java
+++ src/main/java/com/orion/model/User.java
@@ -1,7 +1,16 @@
 package com.orion.model;
 
-import jakarta.persistence.*;
-import lombok.*;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
+import jakarta.persistence.Id;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Column;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 
 @Entity
 @Table(name = "users")
