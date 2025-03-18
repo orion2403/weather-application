8dcd2c15059078cfea7e4ce0d54cee957b47c42b
orion2403 <tembulatbekov7@gmail.com>
2025-03-18 20:54:07 +0300

    feat: add pattern annotation above password for the better validation

--- src/main/java/com/orion/dto/UserRegisterDto.java
+++ src/main/java/com/orion/dto/UserRegisterDto.java
@@ -2,10 +2,22 @@ package com.orion.dto;
 
 import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Pattern;
 import jakarta.validation.constraints.Size;
 
 public record UserRegisterDto(
-        @Email(message = "The email address does not match.") @NotBlank(message = "Login cannot be empty") String login,
-        @Size(min = 8, message = "Password must be at least 8 characters long.") String password,
-        @Size(min = 8, message = "Password must be at least 8 characters long.") String passwordConfirm) {
+
+        @Email(message = "The email address does not match.")
+        @NotBlank(message = "Login cannot be empty")
+        String login,
+
+        @Size(min = 8, message = "Password must be at least 8 characters long.")
+        @Pattern(regexp = "((?=.*\\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[@#$%]).{8,20})",
+                message = "Password must include at least one digit, one lowercase letter, one uppercase letter, and one special character from: @#$%.")
+        String password,
+
+        @Size(min = 8, message = "Password must be at least 8 characters long.")
+        @Pattern(regexp = "((?=.*\\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[@#$%]).{8,20})",
+                message = "Password must include at least one digit, one lowercase letter, one uppercase letter, and one special character from: @#$%.")
+        String passwordConfirm) {
 }
