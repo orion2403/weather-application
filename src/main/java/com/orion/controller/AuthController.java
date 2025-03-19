8f170026dc57fad92ab4e2fb956e6e69d609a050
orion2403 <tembulatbekov7@gmail.com>
2025-03-19 16:44:53 +0300

    feat: change login, register endpoints, add cookie

--- src/main/java/com/orion/controller/AuthController.java
+++ src/main/java/com/orion/controller/AuthController.java
@@ -1,17 +1,20 @@
 package com.orion.controller;
 
+import com.orion.dto.SessionDto;
 import com.orion.dto.UserLoginDto;
 import com.orion.dto.UserRegisterDto;
+import com.orion.service.AuthService;
+import jakarta.servlet.http.Cookie;
+import jakarta.servlet.http.HttpServletResponse;
 import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.validation.BindingResult;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
 
 @Controller
-@RequestMapping("/weather/ap/auth")
+@RequestMapping("/weather/auth")
+@RequiredArgsConstructor
 public class AuthController {
 
     public static final String REGISTER = "register";
@@ -20,32 +23,41 @@ public class AuthController {
 
 
     @GetMapping("/register")
-    public String showRegisterForm(@ModelAttribute UserRegisterDto userDto) {
+    public String showRegisterForm(@ModelAttribute UserRegisterDto userRegisterDto) {
         return "register";
     }
 
     @PostMapping("/register")
-    public String registerUser(@ModelAttribute @Valid UserRegisterDto userDto,
+    public String registerUser(@ModelAttribute @Valid UserRegisterDto userRegisterDto,
                                BindingResult bindingResult) {
         if (bindingResult.hasErrors()) {
-            return "register";
+            return REGISTER;
         }
-        // TODO add service for register user
-        return null;
+        authService.register(userRegisterDto);
+        return "redirect:/weather/auth/login";
     }
 
     @GetMapping("/login")
-    public String showLoginForm(@ModelAttribute UserLoginDto userDto) {
-        return "login";
+    public String showLoginForm(@ModelAttribute UserLoginDto userLoginDto) {
+        return LOGIN;
     }
 
     @PostMapping("/login")
-    public String login(@ModelAttribute @Valid UserLoginDto userDto,
-                        BindingResult bindingResult) {
+    public String login(@CookieValue(value = "session_id", defaultValue = "") String cookie,
+                        @ModelAttribute @Valid UserLoginDto userLoginDto,
+                        BindingResult bindingResult,
+                        HttpServletResponse response) {
         if (bindingResult.hasErrors()) {
-            return "login";
+            return LOGIN;
         }
-        // TODO add service for login user
-        return null;
+        var sessionDto = authService.login(userLoginDto);
+        createCookie(sessionDto, response);
+        return "home";
+    }
+
+    private void createCookie(SessionDto sessionDto, HttpServletResponse response) {
+        var cookie = new Cookie("session_id", sessionDto.id());
+        cookie.setAttribute("expires_at", sessionDto.expiresAt());
+        response.addCookie(cookie);
     }
 }
