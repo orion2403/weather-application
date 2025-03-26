ee6a87f1c02ec705bef43501b6956912b7129fd4
orion2403 <tembulatbekov7@gmail.com>
2025-03-26 21:47:00 +0300

    feat: add dto to transfer data with location api

---
+++ src/main/java/com/orion/dto/weatherapi/LocationApiDto.java
@@ -0,0 +1,26 @@
+package com.orion.dto.weatherapi;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import com.fasterxml.jackson.annotation.JsonProperty;
+import com.orion.model.User;
+
+import java.math.BigDecimal;
+
+@JsonIgnoreProperties(ignoreUnknown = true)
+public record LocationApiDto(
+
+        String name,
+
+        @JsonProperty("lon")
+        BigDecimal latitude,
+
+        @JsonProperty("lat")
+        BigDecimal longitude,
+
+        String country,
+
+        String state,
+
+        User user
+) {
+}
