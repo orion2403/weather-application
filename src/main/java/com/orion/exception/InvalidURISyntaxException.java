86f3c322af064334cd044b5292e814d8f5e02b1c
orion2403 <tembulatbekov7@gmail.com>
2025-03-30 17:55:07 +0300

    feat: add exception which throws when URI syntax error

---
+++ src/main/java/com/orion/exception/InvalidURISyntaxException.java
@@ -0,0 +1,7 @@
+package com.orion.exception;
+
+public class InvalidURISyntaxException extends RuntimeException {
+    public InvalidURISyntaxException(String message) {
+        super(message);
+    }
+}
