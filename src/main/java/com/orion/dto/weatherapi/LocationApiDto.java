c197cf743e5ff1e14608a7e2dd002e2fa0e316cd
orion2403 <tembulatbekov7@gmail.com>
2025-03-30 17:55:55 +0300

    style: add space between fields

--- src/main/java/com/orion/dto/weatherapi/LocationApiDto.java
+++ src/main/java/com/orion/dto/weatherapi/LocationApiDto.java
@@ -2,7 +2,6 @@ package com.orion.dto.weatherapi;
 
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import com.fasterxml.jackson.annotation.JsonProperty;
-import com.orion.model.User;
 
 import java.math.BigDecimal;
 
@@ -10,17 +9,11 @@ import java.math.BigDecimal;
 public record LocationApiDto(
 
         String name,
-
         @JsonProperty("lon")
         BigDecimal latitude,
-
         @JsonProperty("lat")
         BigDecimal longitude,
-
         String country,
-
-        String state,
-
-        User user
+        String state
 ) {
 }
